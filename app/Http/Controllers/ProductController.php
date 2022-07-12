7cca41d766378dfb2b8e5aded41e3f20ef9b8878
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-07-12 22:46:32 +0300

    Added create function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,14 +11,17 @@ class ProductController extends Controller
 
     public $product;
 
-    public function __construct(IProductRepository $product) {
+    public function __construct(IProductRepository $product) 
+    {
+
         $this->product = $product;
+
     }
 
 
 
-    public function index()
-    {
+    public function index() {
+
         // return all products
 
         $products =  $this->product->getAllProducts();
@@ -26,4 +29,51 @@ class ProductController extends Controller
         return view('product.index')->with('products', $products);
 
     }
+
+    public function show($id)
+    {
+        // get single product
+
+        $product = $this->product->getSingleProduct($id);
+        return view('product.show')->with('product', $product);
+    }
+
+    public function create() {
+
+        // create page
+        return view('product.create');
+
+    }
+
+    public function store(Request $request)
+    
+    {
+
+        // validate and store data
+        $request->validate([
+            'picture' => 'required',
+            'title' => 'required',
+            'price' => 'required',
+            'description' => 'required'
+        ]);
+
+
+        //image upload
+
+        $data = $request->all();
+
+        if($image = $request->file('picture')) {
+            $name = time(). '.' .$image->getClientOriginalName();
+            $image->move(public_path('images'), $name);
+            $data['picture'] = "$name";
+        }
+
+        $this->product->createProduct($data);
+
+        return redirect('/products');
+
+    }
+
+
+
 }
